53de219ac165de351b6e4950489b7c72758420fc
songra365 <songsongra@gmail.com>
2026-06-15 16:18:50 +0900

    마이커스텀 수정

--- src/pages/mypage/mycustom/MyCustomList.jsx
+++ src/pages/mypage/mycustom/MyCustomList.jsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from "react";
 import { apiCall } from "../../../service/apiService";
 import { Link, useNavigate } from "react-router-dom";
+import Pagination from '../../../components/layout/Pagination';
 
+import '../../cscenter/faq/FaqList.css';
 
 function MyCustomList(){
 
     const navigate = useNavigate();
     const [cusList, setCusList] = useState([]);
+    /* 현재 페이지 */
     const [currentPage, setCurrentPage] = useState(1);
+    /* 페이지당 개수 */
     const itemPerPage = 10;
+    /* 전체 페이지 */
     const totalPage = Math.ceil(cusList.length / itemPerPage);
+    /* 시작 인덱스 */
     const startIndex = (currentPage - 1) * itemPerPage;
-
+    const pagedList = cusList.slice(startIndex, startIndex+itemPerPage);
+    
     useEffect(()=>{
         async function loadMyCustomList(){
             if(!sessionStorage.getItem('accessToken')){
@@ -30,50 +37,49 @@ function MyCustomList(){
         },[])
 
     return(
-            
-            <table>
+        <div className="cs-faq-wrap">
+            <div className="cs-faq-header">
+                <h2>내 영양제 추천 보기</h2>
+            </div>        
+            <table className="cs-faq-table">
                 <thead>
                     <tr>
-                        <th>설문자ID</th><th>커스텀번호</th><th>제목</th><th>작성일</th>
+                        <th width='15%'>설문자ID</th>
+                        <th width='10%'>커스텀번호</th>
+                        <th width='50%'>제목</th>
+                        <th width='25%'>작성일</th>
                     </tr>
                 </thead>
 
                 <tbody>
-                    {cusList.length > 0 ? (
-                        cusList.map((cus) => (
+                    {pagedList.length > 0 ? (
+                        pagedList.map((cus) => (
                             <tr key={cus.cusId}>
-                                <td>
-                                    {sessionStorage.getItem('loginId')}
-                                </td>
-                                <td>
-                                    {cus.cusId}
-                                </td>
+                                <td>{sessionStorage.getItem('loginId')}</td>
+                                <td>{cus.cusId}</td>
                                 <td>
                                     <Link to={`/v1/cus/result/${cus.cusId}`}>
                                     {cus.surTitle}
                                     </Link>
                                 </td>
-                                <td>
-                                    {cus.crtAt}
-                                </td>
+                                <td>{cus.crtAt}</td>
                             </tr>
                         ))
                     ) : (
-
                         <tr>
-
-                            <td>
-                                '등록된 FAQ가 없습니다.'
-                            </td>
-
+                            <td colSpan='4' className="cs-faq-empty">'등록된 FAQ가 없습니다.'</td>
                         </tr>
-
                     )}
-
                 </tbody>
-
             </table>
+            
+            <Pagination
+                currentPage={currentPage}
+                totalPage={totalPage}
+                onPageChange={setCurrentPage}
+            />
 
+        </div>
     );
 }
 export default MyCustomList;
