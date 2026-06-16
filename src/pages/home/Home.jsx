b0f9a0cd92dc06f3b828796521ab1be527c3b3d7
numee810 <numee810@gmail.com>
2026-06-16 14:51:57 +0900

    홈 배너 수정

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -39,7 +39,7 @@ function Home({ isLoggedIn }) {
             {/* 메인 배너 영역 */}
             <section className="mainBanner">
                 <img
-                    src="/images/VitaPick_MainBanner.png"
+                    src="/images/VitaPick_MainBanner4.png"
                     alt="메인 배너"
                 />
             </section>
