f503dbd9dd76686dec76b80cf1dadaa1cc8ae1a7
qkrwnstj1346 <qkrwnstj1346@gmail.com>
2026-06-17 19:32:30 +0900

    관리자 페이지 [고객센터관리] UI 추가

--- src/service/admin/adminCsCenterApi.js
+++ src/service/admin/adminCsCenterApi.js
@@ -8,6 +8,10 @@ export function getAdminCsFaqs(params) {
     return apiCall.get('/api/admin/cscenter/faqs', { params });
 }
 
+export function createAdminCsFaq(data) {
+    return apiCall.post('/api/admin/cscenter/faqs', data);
+}
+
 export function getAdminCsInquiries(params) {
     return apiCall.get('/api/admin/cscenter/inquiries', { params });
 }
